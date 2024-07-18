7db0ac883a9c08607bcd1650f817e33b3cb2ac59
Nikola Velinov <nikolavelinov65@gmail.com>
2024-07-18 16:38:36 -0700

    added smtp code

--- monitoring/app.py
+++ monitoring/app.py
@@ -1,5 +1,6 @@
 import requests
-
+from smtplib import SMTP
+from email.mime.text import MIMEText
 from flask import Flask
 from apscheduler.schedulers.background import BackgroundScheduler
 from urllib3.exceptions import MaxRetryError
@@ -7,14 +8,34 @@ from requests.exceptions import ConnectionError, Timeout
 
 app = Flask(__name__)
 
+def send_email():
+
+    from_addr = "devops@activity.net"
+    to_addr = "nikolavelinov65@gmail.com"
+
+    subj = "WEBPAGE ERROR"
+
+    message_text = "Fatal error: URL failed to return expected response"
+
+    message = MIMEText(message_text, "plain")
+    message["Subject"] = subj
+    message["From"] = from_addr
+    message["To"] = to_addr
+
+    with SMTP('mail.mysmtp.com', '25') as server:
+        server.login('myuser', 'secret')
+        server.sendmail(from_addr, to_addr, message.as_string())
+
 def monitor_job():
     try:
         res = requests.get("http://web:5000/", timeout=10)
     except (MaxRetryError, ConnectionError, Timeout):
-        print("timeout error")
+        print("connection error")
+        send_email()
         return
     if res.status_code != 200:
-        print("wrong status code error")
+        print("status code error")
+        send_email()
 
 def schedule_job():
     sched = BackgroundScheduler(daemon=True)
